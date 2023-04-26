3445d8cdb487ac41b70759a0e30744cad1f0934a
Sp00nyMan <mrstormproxdd@gmail.com>
2023-04-26 15:27:49 +0200

    job logs

--- start_job.sh
+++ start_job.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sbatch --output=logs/log_resnet_SGD.txt job_script.sh resnet18 SGD exp
-sbatch --output=logs/log_resnet_AdamW.txt job_script.sh resnet18 AdamW cosine
-sbatch --output=logs/log_convnext_SGD.txt job_script.sh convnext SGD exp
-sbatch --output=logs/log_convnext_AdamW.txt job_script.sh convnext AdamW cosine
+sbatch --output=log_resnet_SGD.txt job_script.sh resnet18 SGD exp
+sbatch --output=log_resnet_AdamW.txt job_script.sh resnet18 AdamW cosine
+sbatch --output=log_convnext_SGD.txt job_script.sh convnext SGD exp
+sbatch --output=log_convnext_AdamW.txt job_script.sh convnext AdamW cosine
