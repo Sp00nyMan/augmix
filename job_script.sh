7f3a8b952828b7f17a44d221ee7183e5038fc5c0
Sp00nyMan <mrstormproxdd@gmail.com>
2023-05-01 16:55:52 +0200

    adapt for ConvNeXt

--- job_script.sh
+++ job_script.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 #SBATCH --partition=gpu
 #SBATCH --gpus=1
-#SBATCH --time=5:00:00
+#SBATCH --time=6:00:00
 #SBATCH --nodes=1
-
 
 module load python
 pip install --user -r requirements.txt
-pip freeze
+python3 --version
+
 echo "-----------------------------------------------------------------------------------------"
 
 echo "Job ID: " $SLURM_JOB_ID
 echo "Job Name: " $SLURM_JOB_NAME
 
-python cifar.py --num-workers=1 --model=$1 --optimizer=$2 --scheduler=$3
+python3 cifar.py --num-workers=1 --model=$1 --optimizer=$2 --scheduler=$3
