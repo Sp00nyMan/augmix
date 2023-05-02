f49869fe9a820eb0863f586fd4ff7afe7d8333aa
Sp00nyMan <mrstormproxdd@gmail.com>
2023-05-02 12:27:26 +0200

    Conda

--- job_script.sh
+++ job_script.sh
@@ -4,13 +4,13 @@
 #SBATCH --time=6:00:00
 #SBATCH --nodes=1
 
-module load python
-pip install --user -r requirements.txt
-python3 --version
+module load miniconda3
+conda activate base
+conda install pytorch=2.0.0 torchvision torchaudio pytorch-cuda=11.7 -c pytorch -c nvidia
 
 echo "-----------------------------------------------------------------------------------------"
 
 echo "Job ID: " $SLURM_JOB_ID
 echo "Job Name: " $SLURM_JOB_NAME
 
-python3 cifar.py --num-workers=1 --model=$1 --optimizer=$2 --scheduler=$3
+python cifar.py --num-workers=1 --model=$1 --optimizer=$2 --scheduler=$3
